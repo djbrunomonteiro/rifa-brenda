37be570ae6927b697066ca8bb6278d0b1c9647f6
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2024-03-24 13:59:50 -0300

    start project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"rifa-brenda","appId":"1:249198544691:web:280944029e34281d366046","storageBucket":"rifa-brenda.appspot.com","apiKey":"AIzaSyD9vftHNix5Ice0XNFKDgF2DTKk8t5NwkE","authDomain":"rifa-brenda.firebaseapp.com","messagingSenderId":"249198544691"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), provideAnimationsAsync()]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+
+    firebaseConfig : {
+        apiKey: "AIzaSyD9vftHNix5Ice0XNFKDgF2DTKk8t5NwkE",
+        authDomain: "rifa-brenda.firebaseapp.com",
+        projectId: "rifa-brenda",
+        storageBucket: "rifa-brenda.appspot.com",
+        messagingSenderId: "249198544691",
+        appId: "1:249198544691:web:280944029e34281d366046"
+    }
+};
